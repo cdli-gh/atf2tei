5c9f296eac09afa9052513e9f465c790a80394ad
Ralph Giles <giles@thaumas.net>
2019-06-25 13:38:18 -0700

    Add a simple test for the atf segmentor.
    Just verify that something comes out.

--- test_atf2tei.py
+++ test_atf2tei.py
@@ -1,8 +1,14 @@
 import io
 import atf2tei
+import atf2cts
 
 
 def test_convert():
     with io.open("SIL-034.atf", encoding='utf-8') as f:
         xml = atf2tei.convert(f.read())
         assert xml
+
+
+def test_cts():
+    with io.open("SIL-034.atf", encoding='utf-8') as f:
+        assert len(list(atf2cts.segmentor(f))) == 1
