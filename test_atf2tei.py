669422e5ed5a43beae2ebc6ec4a4e7a6e7d28236
Ralph Giles <giles@thaumas.net>
2019-06-25 13:53:47 -0700

    test: Move the test filename to a variable.
    Avoid repeating the name in each test.

--- test_atf2tei.py
+++ test_atf2tei.py
@@ -3,12 +3,17 @@ import atf2tei
 import atf2cts
 
 
+test_filename = 'SIL-034.atf'
+
+
 def test_convert():
-    with io.open("SIL-034.atf", encoding='utf-8') as f:
+    '''Verify conversion of a test file.'''
+    with io.open(test_filename, encoding='utf-8') as f:
         xml = atf2tei.convert(f.read())
         assert xml
 
 
-def test_cts():
-    with io.open("SIL-034.atf", encoding='utf-8') as f:
+def test_segmentor_single():
+    '''Verify segmentation of a test file.'''
+    with io.open(test_filename, encoding='utf-8') as f:
         assert len(list(atf2cts.segmentor(f))) == 1
