030eeedb296187476a87fac31b36d6671d0cfc4c
Ralph Giles <giles@thaumas.net>
2019-06-25 13:54:21 -0700

    test: Add a paratrized test of the segmentor.
    Verify multiple copies segment into the correct number of atf blocks.
    This is redundant with `test_segmentor_single` but I like having
    the very simple test first since it's easier to understand.

--- test_atf2tei.py
+++ test_atf2tei.py
@@ -1,4 +1,8 @@
 import io
+import itertools
+
+import pytest
+
 import atf2tei
 import atf2cts
 
@@ -17,3 +21,19 @@ def test_segmentor_single():
     '''Verify segmentation of a test file.'''
     with io.open(test_filename, encoding='utf-8') as f:
         assert len(list(atf2cts.segmentor(f))) == 1
+
+
+@pytest.mark.parametrize('count', range(4))
+def test_segmentor(count):
+    '''Verify segmentation of multiple blocks.
+
+    Concatenate the test file with itself and check we get back
+    the name number of copies.'''
+    with io.open(test_filename, encoding='utf-8') as f:
+        text = f.read()
+    assert text
+
+    loop = itertools.repeat(text)
+    multi = '\n\n'.join(itertools.islice(loop, count))
+    multi = io.StringIO(multi)
+    assert (len(list(atf2cts.segmentor(multi)))) == count
