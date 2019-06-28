7f7e5eb35342450e250b2f8617802297d6305ce7
Ralph Giles <giles@thaumas.net>
2019-06-28 08:50:36 -0700

    Move tests to a sub-module.
    Reduces clutter in the top-level directory.
    
    We seem to need the dummy __init__.py to make imports work.

--- cts.py
+++ cts.py
@@ -33,35 +33,6 @@ class TextGroup:
         return xml
 
 
-def test_bare():
-    group = TextGroup()
-    xml = ET.fromstring(str(group))
-    qname = '{' + TextGroup.ns['ti'] + '}textgroup'
-    assert xml.tag == qname
-
-
-def test_urn():
-    urn = 'urn:cts:cdli:test.faketext'
-    group = TextGroup()
-    group.urn = urn
-    xml = ET.fromstring(str(group))
-    assert xml.tag == '{' + TextGroup.ns['ti'] + '}textgroup'
-    assert xml.get('urn') == urn
-
-
-def test_group():
-    name = 'Example Text Group'
-    urn = 'urn:cts:cdli:test.example'
-    group = TextGroup()
-    group.name = name
-    group.urn = urn
-    xml = ET.fromstring(str(group))
-    element = xml.find('ti:groupname', TextGroup.ns)
-    assert element.text == name
-    assert xml.tag == '{' + TextGroup.ns['ti'] + '}textgroup'
-    assert xml.get('urn') == urn
-
-
 class Work:
     '''Represents a TEI work.'''
 
@@ -103,14 +74,3 @@ class Work:
         if self.description:
             description.text = self.description
         return xml
-
-
-def test_work():
-    work = Work()
-    print(work)
-    assert work.xml
-
-
-if __name__ == '__main__':
-    test_group()
-    test_work()
