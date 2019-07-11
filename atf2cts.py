9c17ab45ae753621bf2633a783a1ef123a0e2f5d
Ralph Giles <giles@thaumas.net>
2019-07-11 11:05:27 -0700

    Move cts conversion into a function.
    Cleans up a bit and makes it easier to experiment with the harness.

--- atf2cts.py
+++ atf2cts.py
@@ -22,6 +22,60 @@ def segmentor(fp):
         yield atf
 
 
+def convert(atf, data_path):
+    failed_parse = []
+    failed_export = []
+    success = 0
+
+    try:
+        xml = atf2tei.convert(atf)
+    except Exception as e:
+        print('Error converting ATF:', e)
+        print(atf)
+        failed_parse.append(atf)
+        return (success, failed_parse, failed_export)
+    try:
+        dom = parseString(xml)
+    except Exception as e:
+        print('Error parsing converted XML:', e)
+        print(xml)
+        failed_export.append(xml)
+        return (success, failed_parse, failed_export)
+    texts = dom.getElementsByTagName('text')
+    assert len(texts) == 1
+    text = texts[0]
+    urn = text.getAttribute('n')
+    lang = text.getAttribute('xml:lang')
+    title = dom.getElementsByTagName('title')[0].firstChild.data
+
+    doc_basename = urn.split(':')[-1]
+    doc_dirname = doc_basename.split('.')[-1]
+    doc_path = os.path.join(data_path, doc_dirname)
+    doc_filename = os.path.join(
+            doc_path, doc_basename + '.' + lang + '.xml')
+    print('-- Writing', urn, lang, 'to', doc_filename)
+
+    work = cts.Work()
+    work.group_urn = textgroup.urn
+    work.work_urn = urn
+    work.language = lang
+    work.description = 'Test doc converted from atf.'
+    work.label = ' '.join(['CDLI', doc_dirname, title])
+    work.title = title
+
+    os.makedirs(doc_path, exist_ok=True)
+    with io.open(os.path.join(doc_path, '__cts__.xml'),
+                 encoding='utf-8',
+                 mode='w') as f:
+        f.write(str(work))
+
+    with io.open(doc_filename, encoding='utf-8', mode='w') as f:
+        f.write(xml)
+    success += 1
+
+    return (success, failed_parse, failed_export)
+
+
 if __name__ == '__main__':
     import io
     import os
@@ -47,51 +101,10 @@ if __name__ == '__main__':
         print('-- Parsing:', filename)
         with io.open(filename, encoding='utf-8') as f:
             for atf in segmentor(f):
-                try:
-                    xml = atf2tei.convert(atf)
-                except Exception as e:
-                    print('Error converting ATF:')
-                    print(atf)
-                    failed_parse.append(atf)
-                    continue
-                try:
-                    dom = parseString(xml)
-                except Exception as e:
-                    print('Error parsing converted XML:')
-                    print(xml)
-                    failed_export.append(xml)
-                    continue
-                texts = dom.getElementsByTagName('text')
-                assert len(texts) == 1
-                text = texts[0]
-                urn = text.getAttribute('n')
-                lang = text.getAttribute('xml:lang')
-                title = dom.getElementsByTagName('title')[0].firstChild.data
-
-                doc_basename = urn.split(':')[-1]
-                doc_dirname = doc_basename.split('.')[-1]
-                doc_path = os.path.join(data_path, doc_dirname)
-                doc_filename = os.path.join(
-                        doc_path, doc_basename + '.' + lang + '.xml')
-                print('-- Writing', urn, lang, 'to', doc_filename)
-
-                work = cts.Work()
-                work.group_urn = textgroup.urn
-                work.work_urn = urn
-                work.language = lang
-                work.description = 'Test doc converted from atf.'
-                work.label = ' '.join(['CDLI', doc_dirname, title])
-                work.title = title
-
-                os.makedirs(doc_path, exist_ok=True)
-                with io.open(os.path.join(doc_path, '__cts__.xml'),
-                             encoding='utf-8',
-                             mode='w') as f:
-                    f.write(str(work))
-
-                with io.open(doc_filename, encoding='utf-8', mode='w') as f:
-                    f.write(xml)
-                success += 1
+                s, p, e = convert(atf, data_path)
+                success += s
+                failed_parse.extend(p)
+                failed_export.extend(e)
         failed = len(failed_parse) + len(failed_export)
         ratio = 1 - failed/(failed + success)
         width = 50
